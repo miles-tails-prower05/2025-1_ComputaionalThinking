99310283a314b0a03fbfcdd67eb5a7cccd7a152f
Kunwoo Park <81471509+miles-tails-prower05@users.noreply.github.com>
2025-05-14 11:46:30 +0900

    과제 코드 완성

--- 2025.05.14.py
+++ 2025.05.14.py
@@ -1,4 +1,10 @@
+# 학점계산_202510894_박건우.py
+
 print("평균 학점을 계산해드립니다.")
-math = int(input("수학 점수 입력:"))
-eng = int(input("영어 점수 입력:"))
-print("평균 학점은", (math+eng)/2 , "입니다.")
+
+math = int(input("수학 점수 입력: "))
+eng = int(input("영어 점수 입력: "))
+kor = int(input("국어 점수 입력: "))
+sci = int(input("과학 점수 입력: "))
+
+print("평균 학점은", (math+eng+kor+sci)/4 , "입니다.")
